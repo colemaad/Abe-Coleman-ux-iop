c14b22646e756154b5fc70a718d09e05f9e6a006
Abraham <abraham.coleman@banno.com>
2015-08-04 13:41:15 -0500

    Final for 3.1, 3.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
-
  var gulp = require('gulp'),
   connect = require('gulp-connect');
 
 gulp.task('connect', function() {
   connect.server({
-    root: './',
+    root: 'src',
     port: 1820,
     livereload: true
   });
 });
 
 gulp.task('html', function () {
-  gulp.src('*.html')
+  gulp.src('src')
     .pipe(connect.reload());
 });
 
