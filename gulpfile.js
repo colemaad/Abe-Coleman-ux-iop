a9a9c10026fa7af33eaa6469dc3cc4d199dcaab2
Abraham <abraham.coleman@banno.com>
2015-08-05 13:27:57 -0500

    Basic SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
  var gulp = require('gulp'),
   connect = require('gulp-connect');
+var gulp = require('gulp-help')(require('gulp'));
 
+gulp.task('lint', 'Lints all server side js', function () {
+    gulp.src('src')
+      .pipe(jshint());
+});
 gulp.task('connect', function() {
   connect.server({
     root: 'src',
@@ -14,8 +19,21 @@ gulp.task('html', function () {
     .pipe(connect.reload());
 });
 
+gulp.task('scss', function () {
+  gulp.src('src')
+    .pipe(connect.reload());
+});
+gulp.task('sass', function () {
+  gulp.src('src')
+    .pipe(connect.reload());
+});
+
+
+
 gulp.task('watch', function () {
   gulp.watch(['*.html'], ['html']);
+  gulp.watch(['*.sass'], ['sass']);
+  gulp.watch(['*.scss'], ['scss']);
 });
 
 gulp.task('default', ['connect', 'watch']);
